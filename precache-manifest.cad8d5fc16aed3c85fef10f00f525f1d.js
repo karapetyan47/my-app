d9f5746e3697160b25841e14c917509791e71491
karapetyan47 <karapetyan4747@gmail.com>
2019-11-12 18:46:41 +0400

    Updates

---
+++ precache-manifest.cad8d5fc16aed3c85fef10f00f525f1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c65c5f9650b58e3c38fa08c045787d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a180bc92adb3be5b9dfd",
+    "url": "/my-app/static/css/main.378f73fb.chunk.css"
+  },
+  {
+    "revision": "fc9d3204db2845b26cbd",
+    "url": "/my-app/static/js/2.ed48cd30.chunk.js"
+  },
+  {
+    "revision": "a180bc92adb3be5b9dfd",
+    "url": "/my-app/static/js/main.94c82e41.chunk.js"
+  },
+  {
+    "revision": "3e4f1a0423cdbc62d902",
+    "url": "/my-app/static/js/runtime-main.59ba0a9e.js"
+  },
+  {
+    "revision": "3e841f8184fb69e8c7169598483f3b92",
+    "url": "/my-app/static/media/back.3e841f81.jpg"
+  },
+  {
+    "revision": "244a3e95d2674700d1466c2255833c27",
+    "url": "/my-app/static/media/bee.244a3e95.jpg"
+  },
+  {
+    "revision": "05c01c77569f41a479787d39e8bffae4",
+    "url": "/my-app/static/media/logo.05c01c77.png"
+  }
+]);
